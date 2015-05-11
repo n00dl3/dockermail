2c6d130bfb343d484bfd56227a95f65feb9e9b35
Junius Rendel <junius@jun1u5>
2015-05-11 11:24:28 +0200

    configure on runtime

--- postfixadmin/postfixadmin.sh
+++ postfixadmin/postfixadmin.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 exec 2>&1
 php5enmod imap
+if [ !-e /root/configured ];then
+  if [ -z $DB_NAME] || [ -z $DB_NAME ] || [ -z $DB_USER ] || [ -z $DB_PASSWD ];then
+    echo "you should set every environment variables to run this container"
+  fi
+  SQL="CREATE DATABASE $DB_NAME; GRANT ALL ON $DB_NAME.* to '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWD';"
+  mysql -uroot -p$MYSQL_ROOT_PASSWORD -h mysql -e $sql
+  touch /root/configured
+  unset MYSQL_ROOT_PASSWORD
+fi
 apachectl -DFOREGROUND
