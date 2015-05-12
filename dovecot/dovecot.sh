78a5fdb046a92a4c6f6886ccb0e7b1b68243c756
Junius Rendel <junius@jun1u5>
2015-05-12 15:40:11 +0200

    chown on ssl certs

--- dovecot/dovecot.sh
+++ dovecot/dovecot.sh
@@ -3,5 +3,6 @@ exec 2>&1
 INPUT=/root/dovecot/dovecot-sql.conf.ext
 OUTPUT=/etc/dovecot/dovecot-sql.conf.ext
 sed -e "s/@db_user@/$DB_USER/g" -e "s/@db_name@/$DB_NAME/g" -e "s/@db_password@/$DB_PASSWD/g" $INPUT > $OUTPUT
+chown -R vmail:vmail /srv/ssl
 exec dovecot \
 -F
