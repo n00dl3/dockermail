5a06bd8ce4c023d252f77b9b6577f99195a2bf81
Junius Rendel <junius@jun1u5>
2015-05-12 14:42:25 +0200

    use mysql, it's better for you

--- dovecot/dovecot.sh
+++ dovecot/dovecot.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 exec 2>&1
+INPUT=/root/dovecot/dovecot-sql.conf.ext
+OUTPUT=/etc/dovecot/dovecot-sql.conf.ext
+sed -e "s/@db_user@/$DB_USER/g" -e "s/@db_name@/$DB_NAME/g" -e "s/@db_password@/$DB_PASSWD/g" $INPUT > $OUTPUT
 exec dovecot \
 -F
--- dovecot/postfix.sh
+++ dovecot/postfix.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo $DOMAIN > /etc/mailname
+for file in /root/postfix/*
+do
+  filename = $(basename $file)
+  sed -e "s/@db_user@/$DB_USER/g" -e "s/@db_name@/$DB_NAME/g" -e "s/@db_password@/$DB_PASSWD/g" $file > /etc/postfix/$filename
+done
 command_directory=`postconf -h command_directory`
 daemon_directory=`$command_directory/postconf -h daemon_directory`
 # make consistency check
