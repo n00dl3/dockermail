381a2589a045e6c41598a6a46b2c4e27416184b6
Junius Rendel <junius@jun1u5>
2015-05-13 14:32:46 +0200

    correction dovecot and postfix runit scripts

--- dovecot/dovecot.sh
+++ dovecot/dovecot.sh
@@ -4,8 +4,10 @@ INPUT=/root/dovecot/dovecot-sql.conf.ext
 OUTPUT=/etc/dovecot/dovecot-sql.conf.ext
 for file in /root/dovecot/conf.d/*
 do
-  filename = $(basename $file)
-  sed -e "s/@db_user@/$DB_USER/g" -e "s/@domain@/$DOMAIN/g" -e "s/@db_name@/$DB_NAME/g" -e "s/@db_password@/$DB_PASSWD/g" $file > /etc/dovecot/conf.d/$filename
+  filename=$(basename $file)
+  if [ filename!="." ]&&[ filename!=".." ]
+    sed -e "s/@db_user@/$DB_USER/g" -e "s/@domain@/$DOMAIN/g" -e "s/@db_name@/$DB_NAME/g" -e "s/@db_password@/$DB_PASSWD/g" $file > /etc/dovecot/conf.d/$filename
+  fi
 done
 sed -e "s/@db_user@/$DB_USER/g" -e "s/@db_name@/$DB_NAME/g" -e "s/@domain@/$DOMAIN/g" -e "s/@db_password@/$DB_PASSWD/g" $INPUT > $OUTPUT
 chown -R vmail:vmail /srv/ssl
