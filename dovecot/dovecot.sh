001efc5f883452ed1a1dcf718f9ad5e23ee6ee8c
Junius Rendel <junius@jun1u5>
2015-05-13 14:34:48 +0200

    correction dovecot and postfix runit scripts

--- dovecot/dovecot.sh
+++ dovecot/dovecot.sh
@@ -5,7 +5,7 @@ OUTPUT=/etc/dovecot/dovecot-sql.conf.ext
 for file in /root/dovecot/conf.d/*
 do
   filename=$(basename $file)
-  if [ filename!="." ]&&[ filename!=".." ]
+  if [ filename!="." ]&&[ filename!=".." ];then
     sed -e "s/@db_user@/$DB_USER/g" -e "s/@domain@/$DOMAIN/g" -e "s/@db_name@/$DB_NAME/g" -e "s/@db_password@/$DB_PASSWD/g" $file > /etc/dovecot/conf.d/$filename
   fi
 done
--- dovecot/postfix.sh
+++ dovecot/postfix.sh
@@ -3,7 +3,7 @@ echo $DOMAIN > /etc/mailname
 for file in /root/postfix/*
 do
   filename=$(basename $file)
-  if [ filename!="." ]&&[ filename!=".." ]
+  if [ filename!="." ]&&[ filename!=".." ];then
     sed -e "s/@db_user@/$DB_USER/g" -e "s/@db_name@/$DB_NAME/g" -e "s/@db_password@/$DB_PASSWD/g" -e "s/@domain@/$DOMAIN/g" $file > /etc/postfix/$filename
   fi
 done
