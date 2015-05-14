2789e7e0e27da49f69cabd6417f7e20972de2280
Junius Rendel <junius@jun1u5>
2015-05-14 14:50:21 +0200

    add roundcube

---
+++ roundcube/config.inc.php
@@ -0,0 +1,93 @@
+<?php
+
+/* Local configuration for Roundcube Webmail */
+
+// ----------------------------------
+// SQL DATABASE
+// ----------------------------------
+// Database connection string (DSN) for read+write operations
+// Format (compatible with PEAR MDB2): db_provider://user:password@host/database
+// Currently supported db_providers: mysql, pgsql, sqlite, mssql or sqlsrv
+// For examples see http://pear.php.net/manual/en/package.database.mdb2.intro-dsn.php
+// NOTE: for SQLite use absolute path: 'sqlite:////full/path/to/sqlite.db?mode=0646'
+
+
+  $db_user = getenv("DB_USER")?getenv("DB_USER"):null;
+  $db_passwd=getenv("DB_PASSWD")?getenv("DB_PASSWD"):null;
+  $db_name=getenv("DB_NAME")?getenv("DB_NAME"):null;
+  if(!$db_user||!$db_passwd||!$db_name)
+    throw new Exception("no user or password defined for database connection, please provide one via environment variables DB_USER, DB_NAME and DB_PASSWD");
+  else{
+    $dsn='mysql://'.$db_user.':'.$db_passwd.'@mysql/'.$db_name;
+    $config['db_dsnw'] = $dsn;
+  }
+
+// ----------------------------------
+// IMAP
+// ----------------------------------
+// The mail host chosen to perform the log-in.
+// Leave blank to show a textbox at login, give a list of hosts
+// to display a pulldown menu or set one host as string.
+// To use SSL/TLS connection, enter hostname with prefix ssl:// or tls://
+// Supported replacement variables:
+// %n - hostname ($_SERVER['SERVER_NAME'])
+// %t - hostname without the first part
+// %d - domain (http hostname $_SERVER['HTTP_HOST'] without the first part)
+// %s - domain name after the '@' from e-mail address provided at login screen
+// For example %n = mail.domain.tld, %t = domain.tld
+// WARNING: After hostname change update of mail_host column in users table is
+//          required to match old user data records with the new host.
+
+// $config['default_host'] = 'ssl://your-imap-server';
+$config['default_host'] = '';
+
+// required to ignore SSL cert. verification
+// see: https://bbs.archlinux.org/viewtopic.php?id=187063
+$config['imap_conn_options'] = array(
+  'ssl' => array(
+	 'verify_peer'  => false,
+	 'verify_peer_name' => false,
+   ),
+);
+
+
+// SMTP username (if required) if you use %u as the username Roundcube
+// will use the current username for login
+$config['smtp_user'] = '%u';
+
+// SMTP password (if required) if you use %p as the password Roundcube
+// will use the current user's password for login
+$config['smtp_pass'] = '%p';
+
+// SMTP server just like IMAP server
+
+$config['smtp_server'] = 'tls://%h';
+
+// provide an URL where a user can get support for this Roundcube installation
+// PLEASE DO NOT LINK TO THE ROUNDCUBE.NET WEBSITE HERE!
+$config['support_url'] = 'mailto:postmaster@server';
+
+// use this folder to store log files (must be writeable for apache user)
+// This is used by the 'file' log driver.
+$config['log_dir'] = '/rc/logs';
+
+// use this folder to store temp files (must be writeable for apache user)
+$config['temp_dir'] = '/rc/tmp';
+
+// this key is used to encrypt the users imap password which is stored
+// in the session record (and the client cookie if remember password is enabled).
+// please provide a string of exactly 24 chars.
+$config['des_key'] = hex2bin('{{RANDOM_KEY}}');
+
+// Name your service. This is displayed on the login screen and in the window title
+$config['product_name'] = 'RC Mail';
+
+// ----------------------------------
+// PLUGINS
+// ----------------------------------
+// List of active plugins (in plugins/ directory)
+$config['plugins'] = array();
+
+// the default locale setting (leave empty for auto-detection)
+// RFC1766 formatted language name like en_US, de_DE, de_CH, fr_FR, pt_BR
+$config['language'] = 'en_US';
