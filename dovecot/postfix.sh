3ff6acb38e1e6cf802700f32b523cc87e9d4fceb
Junius Rendel <julien.durens@linkeo.com>
2015-05-15 13:50:30 +0200

    fix runit

--- dovecot/postfix.sh
+++ dovecot/postfix.sh
@@ -7,8 +7,9 @@ do
     sed -e "s/@db_user@/$DB_USER/g" -e "s/@db_name@/$DB_NAME/g" -e "s/@db_password@/$DB_PASSWD/g" -e "s/@domain@/$DOMAIN/g" $file > /etc/postfix/$filename
   fi
 done
-
+command_directory=$(postconf -h command_directory)
+daemon_directory=$($command_directory/postconf -h daemon_directory)
 # make consistency check
 postfix check 2>&1
 # run Postfix
-exec postfix start -v 2>&1
+exec $daemon_directory/master -v 2>&1
