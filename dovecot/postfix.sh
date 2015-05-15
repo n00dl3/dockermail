31c30f24b63859f888a1aac978a4955b32c60cd5
Junius Rendel <julien.durens@linkeo.com>
2015-05-15 12:05:15 +0200

    verbose postfix

--- dovecot/postfix.sh
+++ dovecot/postfix.sh
@@ -7,9 +7,8 @@ do
     sed -e "s/@db_user@/$DB_USER/g" -e "s/@db_name@/$DB_NAME/g" -e "s/@db_password@/$DB_PASSWD/g" -e "s/@domain@/$DOMAIN/g" $file > /etc/postfix/$filename
   fi
 done
-command_directory=`postconf -h command_directory`
-daemon_directory=`$command_directory/postconf -h daemon_directory`
+
 # make consistency check
-$command_directory/postfix check 2>&1
+postfix check 2>&1
 # run Postfix
-exec $daemon_directory/master 2>&1
+exec postfix start -v 2>&1
