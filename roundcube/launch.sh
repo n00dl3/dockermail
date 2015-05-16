abe08cf71de594999c46b465d03b04a92c269143
Junius Rendel <junius@jun1u5>
2015-05-16 18:15:53 +0200

    wrong length

--- roundcube/launch.sh
+++ roundcube/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 exec 2>&1
 if [ ! -e /root/configured ];then
-  DES_KEY=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
+  DES_KEY=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-24};echo;)
   sed -e "s/@DES_KEY@/$DES_KEY/g" /root/config.inc.php > /rc/config.inc.php
   mysql -u$DB_USER -p$DB_PASSWD $DB_NAME</root/roundcubemail.sql
 fi
