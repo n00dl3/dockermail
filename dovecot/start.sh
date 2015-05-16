312203837a2d3df8657e972d2d015fb15f271df3
Junius Rendel <junius@jun1u5>
2015-05-16 16:49:44 +0200

    rework init

--- dovecot/start.sh
+++ dovecot/start.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-exec 2>&1
+#############
+## POSTFIX ##
+#############
+for file in /root/postfix/*
+do
+  filename=$(basename $file)
+  if [ filename!="." ]&&[ filename!=".." ];then
+    sed -e "s/@db_user@/$DB_USER/g" -e "s/@db_name@/$DB_NAME/g" -e "s/@db_password@/$DB_PASSWD/g" -e "s/@domain@/$DOMAIN/g" $file > /etc/postfix/$filename
+  fi
+done
+
+#############
+## DOVECOT ##
+#############
 INPUT=/root/dovecot/dovecot-sql.conf.ext
 OUTPUT=/etc/dovecot/dovecot-sql.conf.ext
 for file in /root/dovecot/conf.d/*
@@ -10,6 +23,13 @@ do
   fi
 done
 sed -e "s/@db_user@/$DB_USER/g" -e "s/@db_name@/$DB_NAME/g" -e "s/@domain@/$DOMAIN/g" -e "s/@db_password@/$DB_PASSWD/g" $INPUT > $OUTPUT
+
+
+####################
+## ACTUAL STARTUP ##
+####################
 chown -R vmail:vmail /srv/ssl
-exec dovecot \
--F
+chown -R vmail:vmail /srv/vmail
+service rsyslog start
+service postfix start
+dovecot -F
